c2199cdaaabe492bc13dcd0a6bb9ae1955d9e05c
Papowe <63293783+Papowe@users.noreply.github.com>
2021-01-23 08:58:00 +0200

    Сделал AxisManager и поправил проверку на столкновения на TryGetComponent

---
+++ Gavran.M_Game/Assets/Scripts/Managers/AxisManager.cs
@@ -0,0 +1,8 @@
+﻿namespace GavranGame
+{
+    public static class AxisManager
+    {
+        public static string HORIZONTAL = "Horizontal";
+        public static string VERTICAL = "Vertical";
+    }
+}
