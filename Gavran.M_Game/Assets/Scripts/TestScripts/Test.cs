83e9e2640df28f2337411c466e603961753bb85d
Papowe <63293783+Papowe@users.noreply.github.com>
2021-01-29 14:26:11 +0200

    Выучил 3 урок

--- Gavran.M_Game/Assets/Scripts/TestScripts/Test.cs
+++ Gavran.M_Game/Assets/Scripts/TestScripts/Test.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.IO;
+using UnityEngine;
 
 namespace GavranGame
 {
@@ -6,9 +8,24 @@ namespace GavranGame
     {
         private void Start()
         {
-           
-            
-            
+            try
+            {
+                Division(5, 0);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+        
+        private void Division(object a, object b)
+        {
+            if (Convert.ToInt32(b) == 0)
+            {
+                throw new Exception("Деление на ноль");
+            }
+            var x = Convert.ToInt32(a) / Convert.ToInt32(b);
+            Debug.Log($"{a} / {b} = {x}");
         }
     }
 }
