155e2b22952afd1e3b32b0438e128f618e4ef9c6
Papowe <63293783+Papowe@users.noreply.github.com>
2021-02-14 12:39:41 +0200

    Пописал примеры методички.

--- Gavran.M_Game/Assets/Scripts/PhotoController.cs
+++ Gavran.M_Game/Assets/Scripts/PhotoController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.IO;
+using GavranGame;
 using UnityEngine;
 
 public sealed class PhotoController
--- Gavran.M_Game/Assets/Scripts/TestScripts/Test.cs
+++ Gavran.M_Game/Assets/Scripts/TestScripts/Test.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace GavranGame
 {
     public class Test : MonoBehaviour
     {
-        private void Start()
-        {
-            // PhotoController _photoController = new PhotoController();
-            
-            // StartCoroutine(_photoController.DoTapExampleAsync());
-            // _photoController.FirstMethod();
-            
-            ///////// Результат двух скриншотов в папке daraSave ////////////
-        }
+       
     }
 }
